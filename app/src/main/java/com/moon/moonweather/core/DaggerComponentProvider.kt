d5fc679e7e13454e72bacb3314ece56b48b39d83
ikakus <idadiani88@gmail.com>
2021-02-21 10:40:49 +0200

    Add DaggerComponentProvider

---
+++ app/src/main/java/com/moon/moonweather/core/DaggerComponentProvider.kt
@@ -0,0 +1,28 @@
+package com.moon.moonweather.core
+
+import java.lang.ref.WeakReference
+
+abstract class DaggerComponentProvider<T> {
+    private var component: T? = null
+    private var subScopes: MutableSet<WeakReference<DaggerComponentProvider<*>>> = mutableSetOf()
+
+    fun get(): T {
+        if (component == null) {
+            component = buildComponent()
+        }
+        return component!!
+    }
+
+    fun dependAndGet(subScope: DaggerComponentProvider<*>): T? {
+        subScopes.add(WeakReference(subScope))
+        return get()
+    }
+
+    protected abstract fun buildComponent(): T
+
+    fun destroy() {
+        subScopes.forEach { it.get()?.destroy() }
+        subScopes.clear()
+        component = null
+    }
+}
