b9513c4a4047255e45a83fcec9bf53393a788601
ikakus <idadiani88@gmail.com>
2021-02-27 09:15:48 +0200

    Use weak reference

--- app/src/main/java/com/moon/moonweather/App.kt
+++ app/src/main/java/com/moon/moonweather/App.kt
@@ -2,13 +2,14 @@ package com.moon.moonweather
 
 import android.app.Application
 import com.moon.moonweather.componentprovider.app.AppComponentProvider
+import java.lang.ref.WeakReference
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-        AppComponentProvider.context = this
+        AppComponentProvider.context = WeakReference(this)
         AppComponentProvider.get().inject(this)
     }
 }
--- app/src/main/java/com/moon/moonweather/componentprovider/app/AppComponentProvider.kt
+++ app/src/main/java/com/moon/moonweather/componentprovider/app/AppComponentProvider.kt
@@ -3,15 +3,16 @@ package com.moon.moonweather.componentprovider.app
 import android.annotation.SuppressLint
 import android.content.Context
 import com.moon.moonweather.core.di.DaggerComponentProvider
+import java.lang.ref.WeakReference
 
 @SuppressLint("StaticFieldLeak")
 object AppComponentProvider : DaggerComponentProvider<AppComponent>() {
 
-    lateinit var context: Context
+    lateinit var context: WeakReference<Context>
 
     override fun buildComponent(): AppComponent {
         return DaggerAppComponent.builder()
-            .provideContext(context)
+            .provideContext(requireNotNull(context.get()))
             .build()
     }
 }
